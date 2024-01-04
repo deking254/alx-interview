660f701fb36354b7943b087a180cc693803c89f7
deking254 <dekingsky@gmail.com>
2024-01-04 14:25:31 +0000

    removed the data checker

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,9 +7,8 @@ def validUTF8(data):
     returns true or false depending on the validity
     of the data utf8 encoding
     """
-    if data:
-        try:
-            b = bytes(data).decode('utf-8')
-            return True
-        except Exception:
-            return False
+    try:
+        b = bytes(data).decode('utf-8')
+        return True
+    except Exception:
+        return False

95d3805cad494898b26f28005413b7bb3827e53a
deking254 <dekingsky@gmail.com>
2024-01-21 16:09:39 +0000

    hopefully my last commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,12 +1,25 @@
 #!/usr/bin/node
-let request = require('request')
-function get_characters(){
-	request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (t, ty)=>{
-	let characters = JSON.parse(ty.body).characters;
-	for (i=0; i<=characters.length; i++){
-		request.get(characters[i], (res, response)=>{
-			console.log(JSON.parse(response.body).name);
-	})
-	}
-})}
-get_characters()
+const request = require('request');
+let index = 0;
+let characterz = [];
+function GetPerson (character) {
+  request.get(character, (error, res, body) => {
+    if (error) {
+      console.log('wer');
+      GetPerson(character);
+    } else {
+      console.log(JSON.parse(res.body).name);
+      index = index + 1;
+      if (index < characterz.length) {
+        GetPerson(characterz[index]);
+      }
+    }
+  });
+}
+function GetCharacters () {
+  request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (t, ty) => {
+    characterz = JSON.parse(ty.body).characters;
+    GetPerson(characterz[0]);
+  });
+}
+GetCharacters();

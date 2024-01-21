928c36b2ce3cd687de998972cd469af3c37fb7c9
deking254 <dekingsky@gmail.com>
2024-01-21 15:23:05 +0000

    initial commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-var request = require('request')
+let request = require('request')
 function get_characters(){
 	request.get('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (t, ty)=>{
-	var characters = JSON.parse(ty.body).characters;
+	let characters = JSON.parse(ty.body).characters;
 	for (i=0; i<=characters.length; i++){
 		request.get(characters[i], (res, response)=>{
 			console.log(JSON.parse(response.body).name);
